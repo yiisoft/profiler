2b66c6cc3dc89e63610cf51e5227649de1712ddb
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-12-03 14:24:02 +0500

    Improve LogTarget

--- tests/LogTargetTest.php
+++ tests/LogTargetTest.php
@@ -28,4 +28,55 @@ class LogTargetTest extends TestCase
 
         $this->assertEquals($logMessages[$logLevel][$token], $message);
     }
+
+    public function testCollect(): void
+    {
+        $logLevel = 'test*';
+        $category = 'test-message';
+        $token = 'test-token';
+
+        $target = new LogTarget($this->logger, $logLevel);
+
+        $message = [
+            'category' => $category,
+            'token' => $token,
+            'beginTime' => 123,
+            'endTime' => 321,
+            'time' => 123,
+        ];
+
+        $target->collect([$message]);
+        $logMessages = $this->logger->getMessages();
+
+        $this->assertEquals($logMessages[$category][$token], $message);
+    }
+
+    public function testCollectWithExceptCategory(): void
+    {
+        $logLevel = 'test-level';
+        $token = 'test-token';
+
+        $target = new LogTarget($this->logger, $logLevel);
+        $target->except = ['test*'];
+
+        $message = [
+            'category' => $logLevel,
+            'token' => $token,
+            'beginTime' => 123,
+            'endTime' => 321,
+            'time' => 123,
+        ];
+
+        $target->collect([$message]);
+        $logMessages = $this->logger->getMessages();
+
+        $this->assertEmpty($logMessages);
+    }
+
+    public function testGetLogLevel(): void
+    {
+        $target = new LogTarget($this->logger, 'test');
+
+        $this->assertEquals('test', $target->getLogLevel());
+    }
 }
