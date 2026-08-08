aa50d79856865aed8134a487637016b482a4bba1
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-08 08:21:10 +0300

    Add Composer Dependency Analyser

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration())
+    ->disableComposerAutoloadPathScan()
+    ->setFileExtensions(['php'])
+    ->addPathToScan(__DIR__ . '/config', isDev: false)
+    ->addPathToScan(__DIR__ . '/src', isDev: false)
+    ->addPathToScan(__DIR__ . '/tests', isDev: true)
+    // config/events-web.php references an event class from yiisoft/yii-http purely as an
+    // optional integration hook (array key); it's not a real dependency of this package.
+    ->ignoreUnknownClasses(['Yiisoft\Yii\Http\Event\AfterEmit'])
+    ->ignoreErrorsOnPackages(['yiisoft/aliases'], [ErrorType::DEV_DEPENDENCY_IN_PROD]);
